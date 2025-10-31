80dda7e450e6dafd223394a074f78f3808c453a5
Code-With-Khushiii <khushipal20706@gmail.com>
2025-10-31 23:41:50 +0530

    add changes

--- src/components/TestimonialsStatic.tsx
+++ src/components/TestimonialsStatic.tsx
@@ -38,7 +38,7 @@ export default function TestimonialsStatic() {
         setLoaded(true)
     }, [])
     return (
-        <section className="relative min-h-screen bg-gradient-to-br from-orange-400 via-orange-500 to-red-500 overflow-hidden py-20 px-6">
+        <section className="relative min-h-screen bg-gradient-to-br from-orange-400 via-orange-500 to-red-500 overflow-hidden mt-6 px-6">
             {/* <div className="absolute inset-0 " /> */}
 
             {/* Content Container */}
